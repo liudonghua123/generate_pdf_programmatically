7ada1afcb4df17a89adb26451ad84ae205d5c8fa
liudonghua123 <liudonghua123@gmail.com>
2017-03-23 14:23:16 +0800

    initial commit

---
+++ src/main/java/com/liudonghua/demo/generate_pdf_programmatically/Application.java
@@ -0,0 +1,13 @@
+package com.liudonghua.demo.generate_pdf_programmatically;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
